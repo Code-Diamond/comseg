aace5014978a7da2d9164eee6c25e5817d2e1e24
code diamond <KingJamin@hotmail.com>
2021-03-03 08:54:11 -0600

    Update videoPlayer.py
    code cleanup. added video player into a tk window. removed sleep timer. made exit key more robust

--- videoPlayer.py
+++ videoPlayer.py
@@ -1,10 +1,20 @@
+import os
 import vlc
 import time
 import keyboard
+import tkinter as tk
+
+#create tk frame
+root = tk.Tk()
+root.title("ComSeg")
+root.videoFrame = tk.Frame(root)
+root.canvas = tk.Canvas(root.videoFrame).pack(fill=tk.BOTH,expand=1)
+root.videoFrame.pack(fill=tk.BOTH,expand=1)
 
 #create vlc instance
 instance = vlc.Instance()
 player = instance.media_player_new()
+player.set_hwnd(root.videoFrame.winfo_id()) #put in tkinter
 media = instance.media_new("MY_VIDEO.mp4") #specify video
 media.get_mrl()
 player.set_media(media)
@@ -27,19 +37,24 @@ def playPauseButton():
 def skipFive():
 	global player
 	player.set_time(player.get_time() + 5000)
+	print(">>Fast Forwarding>>")
 
 def rewFive():
 	global player
 	player.set_time(player.get_time() - 5000)
+	print("<<Rewinding<<")
+
+def quit():
+	os._exit(1)
 
 # configure hotkeys
 keyboard.on_press_key(" ", lambda _:playPauseButton())
 keyboard.on_press_key(".", lambda _:skipFive())
 keyboard.on_press_key(",", lambda _:rewFive())
+keyboard.on_press_key("x", lambda _:quit())
 
 # detect hotkeys while running
 while True:
-	if keyboard.is_pressed('x'):                                # exit keybind to x
-		exit()
-	else:
-		time.sleep(.1)
+	tk.mainloop()
+
+
